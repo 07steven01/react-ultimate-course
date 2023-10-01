af6cd9abb6d203b1a8a51d7cd7a1150df2925011
Marek Oszczyk <marek.oszczyk@atendesoftware.pl>
2023-10-01 22:05:14 +0200

    05-fragments

--- 03-pizza-menu/03-pizza-menu-marek/src/index.js
+++ 03-pizza-menu/03-pizza-menu-marek/src/index.js
@@ -72,12 +72,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      {pizzas.length > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+
+      {pizzas.length > 0 ? ( // below <> could also be <React.Fragment> when key attribute is needed, e.g. for lists
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later!</p>
       )}
