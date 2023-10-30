926941f3197fedf934e48bf7f95e9dcbaf188aca
Marek <ho-hum@o2.pl>
2023-10-30 10:11:35 +0100

    eat-n-split split form add key fix data carryover

--- 06-eat-n-split/06-eat-n-split-marek/src/App.js
+++ 06-eat-n-split/06-eat-n-split-marek/src/App.js
@@ -68,7 +68,11 @@ export default function App() {
         </Button>
       </div>
       {selectedFriend && (
-        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} />
+        <FormSplitBill
+          key={selectedFriend.id}
+          friend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
       )}
     </div>
   );
