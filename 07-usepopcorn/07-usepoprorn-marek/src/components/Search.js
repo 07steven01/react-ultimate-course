bff3173da432483dbc7182f2899e07f0107053c8
07steven01 <ho-hum@o2.pl>
2023-11-21 23:11:01 +0100

    13-useRef-focus-on-mount

--- 07-usepopcorn/07-usepoprorn-marek/src/components/Search.js
+++ 07-usepopcorn/07-usepoprorn-marek/src/components/Search.js
@@ -1,4 +1,13 @@
+import { useEffect, useRef } from "react";
+
 export function Search({ query, setQuery }) {
+  const inputElement = useRef(null); // reference to...
+
+  useEffect(function () {
+    console.log(inputElement.current);
+    inputElement.current.focus();
+  }, []);
+
   return (
     <input
       className="search"
@@ -6,6 +15,7 @@ export function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement} // ... the DOM element
     />
   );
 }
